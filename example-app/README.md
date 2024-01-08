0b617ebfea274d45707cb8173dd17d625cd34672
koreanstudent <hn12344@naver.com>
2024-01-08 10:17:18 +0900

    first commit

---
+++ example-app/README.md
@@ -0,0 +1,7 @@
+프로젝트 구성 순서
+npm i @types/node @types/react @types/react-dom @types/jest
+
+tsconfig.json 생성
+
+모든 js -> jsx
+개별 하나씩 tsx 확장자 변경
---
+++ example-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+const [text, setText] = useState("");
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default App;
